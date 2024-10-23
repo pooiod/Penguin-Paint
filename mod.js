fa1e1fd6b4aabb24fecd17ae65dd5faf0f3278c1
pooiod7 <82846639+pooiod@users.noreply.github.com>
2024-10-23 13:59:21 -0400

    s

--- mod.js
+++ mod.js
@@ -502,7 +502,7 @@ function openPaint() {
         element.style.borderBottomLeftRadius = '15px';
         element.style.width = "1px";
         element.style.paddingRight = "30px";
-        element.style.paddingBottom = "-5px";
+        element.style.paddingBottom = "-10px";
         // element.style.paddingTop = "5px";
         element.style.height = "30px";
     }, 3000); 
