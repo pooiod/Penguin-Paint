488ac557e44af82e460decaf2f52b052535fb85f
pooiod7 <82846639+pooiod@users.noreply.github.com>
2024-11-11 12:56:39 -0500

    d

--- mod.js
+++ mod.js
@@ -3,6 +3,30 @@ document.title = newtitle;
 
 // Fun fact: you can load into penguinpaint on penguinmod by loading it as an unsandboxed extension (no addon support)
 
+const HistoryReplaceState = history.replaceState;
+window.history.replaceState = function(state, title, url) {
+    setTimeout(extrabuttons, 100);
+    try {
+        var img = document.querySelector('#react-tabs-3 > div > div.selector_wrapper_8_BHs.box_box_2jjDp > div.selector_new-buttons_2qHDd.box_box_2jjDp > div > button > img');
+        img.src = "https://cdn-icons-png.flaticon.com/512/4211/4211763.png";
+        img.style.filter = "invert(1)";
+    } catch(err) {
+        console.warn("unable to set image src for button");
+    }
+};
+
+const HistoryPushState = history.pushState;
+window.history.pushState = function(state, title, url) {
+    setTimeout(extrabuttons, 100);
+    try {
+        var img = document.querySelector('#react-tabs-3 > div > div.selector_wrapper_8_BHs.box_box_2jjDp > div.selector_new-buttons_2qHDd.box_box_2jjDp > div > button > img');
+        img.src = "https://cdn-icons-png.flaticon.com/512/4211/4211763.png";
+        img.style.filter = "invert(1)";
+    } catch(err) {
+        console.warn("unable to set image src for button");
+    }
+};
+
 var sidebarcontext = [
     {
         label: "export all",
@@ -842,27 +866,3 @@ const waitForElement = (selector) => {
 };
 
 waitForElement('#react-tabs-2');
-
-const HistoryReplaceState = history.replaceState;
-history.replaceState = function(state, title, url) {
-    setTimeout(extrabuttons, 100);
-    try {
-        var img = document.querySelector('#react-tabs-3 > div > div.selector_wrapper_8_BHs.box_box_2jjDp > div.selector_new-buttons_2qHDd.box_box_2jjDp > div > button > img');
-        img.src = "https://cdn-icons-png.flaticon.com/512/4211/4211763.png";
-        img.style.filter = "invert(1)";
-    } catch(err) {
-        console.warn("unable to set image src for button");
-    }
-};
-
-const HistoryPushState = history.pushState;
-history.pushState = function(state, title, url) {
-    setTimeout(extrabuttons, 100);
-    try {
-        var img = document.querySelector('#react-tabs-3 > div > div.selector_wrapper_8_BHs.box_box_2jjDp > div.selector_new-buttons_2qHDd.box_box_2jjDp > div > button > img');
-        img.src = "https://cdn-icons-png.flaticon.com/512/4211/4211763.png";
-        img.style.filter = "invert(1)";
-    } catch(err) {
-        console.warn("unable to set image src for button");
-    }
-};
