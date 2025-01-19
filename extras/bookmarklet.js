1151c2524beb401bc5b0bfcbfab0809eaba629f9
pooiod7 <82846639+pooiod@users.noreply.github.com>
2025-01-18 21:55:44 -0500

    f

--- extras/bookmarklet.js
+++ extras/bookmarklet.js
@@ -16,3 +16,29 @@ function checkForNewImages() {
 }
 
 setInterval(checkForNewImages, 3000);
+
+if (window.location.hostname === 'studio.penguinmod.com') {
+    let checks = 0;
+    let clicks = 0;
+    let interval = setInterval(() => {
+        if (checks >= 1000) {
+            clearInterval(interval);
+            return;
+        }
+
+        let targetElement = document.querySelector('p.url_url_3Y61f');
+        if (targetElement && (targetElement.textContent.includes('https://raw.githubusercontent.com/pooiod/Penguin-Paint'))) {
+            let button = document.querySelector('button.security-manager-modal_allow-button_3tcXk');
+            if (button) {
+                if (button.disabled) button.disabled = false;
+                button.click();
+                clicks++;
+
+                if (!document.body.contains(targetElement) || clicks >= 10) {
+                    clearInterval(interval);
+                }
+            }
+        }
+        checks++;
+    }, 100);
+}
