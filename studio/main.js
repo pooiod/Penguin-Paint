d94877c4a8a2faee9c6e33ea04850481bd926af7
pooiod7 <82846639+pooiod@users.noreply.github.com>
2025-01-18 20:15:05 -0500

    e

--- studio/main.js
+++ studio/main.js
@@ -26,7 +26,7 @@
                 top: 0;
                 left: 0;
                 width: 100%;
-                height: 100%;
+                height: 30%;
                 display: flex;
                 flex-direction: column;
                 justify-content: center;
